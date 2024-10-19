12e02b33b02b8a1f504d4754fda5269ee0ac119e
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-10-19 09:11:13 +0530

    tests added

---
+++ test.js
@@ -0,0 +1,14 @@
+
+function add(a, b) {
+    console.log("Test 1 passed ")
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    console.log("Test 2 passed ")
+    return a - b;
+  }
+  
+add(2,3);
+subtract(2,3);
+  
